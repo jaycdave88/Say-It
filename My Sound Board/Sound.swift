22c0c48ba06da17b73df69ac119de8790fc483a4
Jay Dave <jaycdave@gmail.com>
2015-06-07 15:03:44 -0700

    created a object for sounds added it to be dyntamic for each cell

---
+++ My Sound Board/Sound.swift
@@ -0,0 +1,14 @@
+//
+//  Sound.swift
+//  My Sound Board
+//
+//  Created by DEV MODE on 6/7/15.
+//  Copyright (c) 2015 DEV MODE. All rights reserved.
+//
+
+import Foundation
+
+class Sound{ // creating a object to have the two properties of name and URL
+    var name = "meow"
+    var URL = NSURL()
+}
--- My Sound Board/SoundListViewController.swift
+++ My Sound Board/SoundListViewController.swift
@@ -6,24 +6,37 @@
 //  Copyright (c) 2015 DEV MODE. All rights reserved.
 //
 
-import UIKit
-import AVFoundation
+import UIKit // apples code for all UI related code
+import AVFoundation // apple's code for audio and video
 
 class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var audioPlayer = AVAudioPlayer()
+    var audioPlayer = AVAudioPlayer() // creating an audio player property as the variable audioplayer
+    
+    var sounds : [Sound] = [] // creating an array to hold all sounds, in swift you need to tell the array what it will be holding, this is done by ": [Sound]" this tells the array that it will contain Sound objects 
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.dataSource = self // added to make the Table view work
         self.tableView.delegate = self // added to make the Table view work
+        
+        var soundPath = NSBundle.mainBundle().pathForResource("testing", ofType: "m4a")// where the file is located
+        
+        var soundURL = NSURL.fileURLWithPath(soundPath!) // creates a url based on the path provided via sound path var
+        
+        var sound1 = Sound() // creating a new sound object 
+        sound1.name = "testing" // providing a name
+        sound1.URL = soundURL! // providing a sound URl
+        
+        self.sounds.append(sound1)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5 // # of rows we want the table view to have
+        return self.sounds.count // # of rows we want the table view to have based on the contents of sounds array
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = UITableViewCell() // each cell on the table
