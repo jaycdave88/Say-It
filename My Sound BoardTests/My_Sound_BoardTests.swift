4b97386cebd00166e943e09cf472d178448e8e67
Jay Dave <jaycdave@gmail.com>
2015-06-07 11:59:13 -0700

    first commit pushing new project to github - added a readme file

---
+++ My Sound BoardTests/My_Sound_BoardTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Sound_BoardTests.swift
+//  My Sound BoardTests
+//
+//  Created by DEV MODE on 6/7/15.
+//  Copyright (c) 2015 DEV MODE. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class My_Sound_BoardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
